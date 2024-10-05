9d48a9b58e737a2f997c60b9deabd08a5aab68de
Дмитрий Малюгин <d.malygin@iqdev.digital>
2024-10-05 14:57:17 +0500

    feat: finish with wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import os from 'os';
+import { Worker } from 'node:worker_threads';
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+
 const performCalculations = async () => {
-    // Write your code here
+    const filename = fileURLToPath(import.meta.url);
+    const dirPath = dirname(filename);
+    const workerPath = path.join(dirPath, '/worker.js');
+    const CPUCores = os.cpus();
+    const CPUCoresNumber = CPUCores.length;
+    const promises = [];
+    const result = [];
+
+    for (let i = 0; i < CPUCoresNumber; i++) {
+        promises.push(await new Promise( (resolve, reject) => {
+            const worker = new Worker(workerPath);
+            worker.postMessage(i + 10);
+            worker.on('message', (workerMessage) => {
+                result.push({
+                    status: 'received',
+                    value: workerMessage
+                });
+                resolve();
+            })
+            worker.on('error', () => {
+                result.push({
+                    status: 'error',
+                    value: null
+                });
+                reject();
+            })
+        }));
+    }
+    Promise.all(promises).then(() => console.log('Result: ', result));
 };
 
 await performCalculations();
