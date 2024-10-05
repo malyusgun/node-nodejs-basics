50060c93cf034e0dbcf3e6dce8653fccb5eb47c0
Дмитрий Малюгин <d.malygin@iqdev.digital>
2024-10-05 14:57:01 +0500

    feat: finish with hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import { createHash } from 'node:crypto';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filename = fileURLToPath(import.meta.url);
+    const dirPath = dirname(filename);
+    const filePath = path.join(dirPath, '/files/fileToCalculateHashFor.txt');
+    let fileData;
+    await fs.readFile(filePath, (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        fileData = data.toString();
+        const hash = createHash('sha256');
+        const fileHash = hash.update(fileData);
+        const fileHashHex = fileHash.digest('hex');
+        console.log('Hash of "fileToCalculateHashFor" file: ', fileHashHex);
+    });
 };
 
 await calculateHash();
